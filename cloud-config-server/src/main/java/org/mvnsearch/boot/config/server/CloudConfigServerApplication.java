74b666a46b5cd79b6a11ae73f0a40d7dd554c7b8
linux_china <linux_china@hotmail.com>
2015-08-28 10:48:32 +0800

    cloud config server application

---
+++ cloud-config-server/src/main/java/org/mvnsearch/boot/config/server/CloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.boot.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigServerApplication.class, args);
+    }
+}
