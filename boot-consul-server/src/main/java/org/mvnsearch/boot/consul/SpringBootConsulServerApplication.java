7786f3ce123e0021020a8bad6cfc3dbeeff68d21
linux_china <linux_china@hotmail.com>
2016-03-25 17:23:19 +0800

    consul config server

---
+++ boot-consul-server/src/main/java/org/mvnsearch/boot/consul/SpringBootConsulServerApplication.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.boot.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsulServerApplication.class, args);
+    }
+}
