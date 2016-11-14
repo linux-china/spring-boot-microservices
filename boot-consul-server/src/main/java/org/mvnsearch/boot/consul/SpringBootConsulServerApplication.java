61f9363b5f1f78ceed6c823fe3f54db2c07f6d06
linux_china <linux_china@hotmail.com>
2016-11-14 13:38:03 +0800

    add EnableDiscoveryClient

--- boot-consul-server/src/main/java/org/mvnsearch/boot/consul/SpringBootConsulServerApplication.java
+++ boot-consul-server/src/main/java/org/mvnsearch/boot/consul/SpringBootConsulServerApplication.java
@@ -2,8 +2,10 @@ package org.mvnsearch.boot.consul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootConsulServerApplication {
 
     public static void main(String[] args) {
