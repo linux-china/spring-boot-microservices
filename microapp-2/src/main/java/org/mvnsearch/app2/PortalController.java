e2fa9d6b094102beda46978e261eabef58c39ff4
linux_china <linux_china@hotmail.com>
2016-12-11 13:59:27 +0800

    add metrics

--- microapp-2/src/main/java/org/mvnsearch/app2/PortalController.java
+++ microapp-2/src/main/java/org/mvnsearch/app2/PortalController.java
@@ -1,6 +1,10 @@
 package org.mvnsearch.app2;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class PortalController {
     @Value("${developer}")
     private String nick;
+    @Autowired
+    private CounterService counterService;
+    @Autowired
+    private MetricRegistry registry;
 
     @GetMapping("/")
     public String welcome() {
+        counterService.increment("counter.page.welcome");
+        Counter evictions = registry.counter("dropwizard.page.welcome");
+        evictions.inc(2);
         return "hello" + nick;
     }
 }
