567d5e65dffe67f3e1b5d8d260b4929fcb680e51
linux_china <linux_china@hotmail.com>
2016-12-11 10:29:26 +0800

    add @EnableDiscoveryClient

--- microapp-1/src/main/java/com/qishon/Microapp1Application.java
+++ microapp-1/src/main/java/com/qishon/Microapp1Application.java
@@ -2,11 +2,13 @@ package com.qishon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Microapp1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Microapp1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Microapp1Application.class, args);
+    }
 }
