3576eed053879e052b5005248e59bc96b1a170a1
linux_china <linux_china@hotmail.com>
2016-12-11 13:32:38 +0800

    consul web server test

---
+++ boot-consul-server/src/test/java/org/mvnsearch/boot/consul/SpringBootConsulServerApplicationTests.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.boot.consul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootConsulServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
