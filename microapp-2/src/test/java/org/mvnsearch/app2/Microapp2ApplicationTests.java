dff1ba0a5a34824fb254f2e5c27ff44ec7a6f15f
linux_china <linux_china@hotmail.com>
2016-12-11 13:29:58 +0800

    micro app2 test

---
+++ microapp-2/src/test/java/org/mvnsearch/app2/Microapp2ApplicationTests.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.app2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mvnsearch.app2.Microapp2Application;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Microapp2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
