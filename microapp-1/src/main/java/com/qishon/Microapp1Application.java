4b267a1270d8b555b775ae63533fcda188913f0f
linux_china <linux_china@hotmail.com>
2016-12-11 09:37:43 +0800

    micro app application

---
+++ microapp-1/src/main/java/com/qishon/Microapp1Application.java
@@ -0,0 +1,12 @@
+package com.qishon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microapp1Application.class, args);
+	}
+}
