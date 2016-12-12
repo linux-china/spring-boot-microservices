cfe962ce70748d3f8e2019f55dc872ac90616b94
linux_china <linux_china@hotmail.com>
2016-12-12 09:52:28 +0800

    sidecar application test

---
+++ sidecar-1/src/test/java/org/mvnsearch/Sidecar1ApplicationTests.java
@@ -0,0 +1,19 @@
+package org.mvnsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class Sidecar1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
