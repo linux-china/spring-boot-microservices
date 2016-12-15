2484042a462c63e55ccf8383093f724df9018243
linux_china <linux_china@hotmail.com>
2016-12-15 17:01:52 +0800

    add @EnableDiscoveryClient

--- cloud-config-server/src/main/java/org/mvnsearch/boot/config/server/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/org/mvnsearch/boot/config/server/CloudConfigServerApplication.java
@@ -2,10 +2,12 @@ package org.mvnsearch.boot.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class CloudConfigServerApplication {
 
     public static void main(String[] args) {
