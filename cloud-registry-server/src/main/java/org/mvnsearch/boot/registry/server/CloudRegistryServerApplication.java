f7b947b1ed96850d2f4f16574957c7a6d44645a2
linux_china <linux_china@hotmail.com>
2016-12-16 16:41:30 +0800

    Polishing

--- cloud-registry-server/src/main/java/org/mvnsearch/boot/registry/server/CloudRegistryServerApplication.java
+++ cloud-registry-server/src/main/java/org/mvnsearch/boot/registry/server/CloudRegistryServerApplication.java
@@ -2,7 +2,6 @@ package org.mvnsearch.boot.registry.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
