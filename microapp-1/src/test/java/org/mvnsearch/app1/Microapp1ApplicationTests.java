b7f02fcf6ebcf6eb86ddaf56e06a3fea7749274c
linux_china <linux_china@hotmail.com>
2017-02-03 12:26:56 +0800

    polishing

--- microapp-1/src/test/java/org/mvnsearch/app1/Microapp1ApplicationTests.java
+++ microapp-1/src/test/java/org/mvnsearch/app1/Microapp1ApplicationTests.java
@@ -2,12 +2,8 @@ package org.mvnsearch.app1;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mvnsearch.app1.Microapp1Application;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
