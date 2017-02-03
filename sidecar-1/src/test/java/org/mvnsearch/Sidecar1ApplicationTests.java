ce1519b8d6f13df6d6498efeef5288789849fb35
linux_china <linux_china@hotmail.com>
2017-02-03 12:28:31 +0800

    Polishing

--- sidecar-1/src/test/java/org/mvnsearch/Sidecar1ApplicationTests.java
+++ sidecar-1/src/test/java/org/mvnsearch/Sidecar1ApplicationTests.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
