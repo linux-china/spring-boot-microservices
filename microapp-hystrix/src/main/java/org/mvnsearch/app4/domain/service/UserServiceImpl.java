4aad2a429f1963d29a9b3ce2f7fabf111913ef67
linux_china <linux_china@hotmail.com>
2017-12-22 12:42:41 +0800

    user service implementation

---
+++ microapp-hystrix/src/main/java/org/mvnsearch/app4/domain/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package org.mvnsearch.app4.domain.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.stereotype.Service;
+
+/**
+ * user service implementation
+ *
+ * @author linux_china
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private CounterService counterService;
+
+    @Override
+    @HystrixCommand(fallbackMethod = "nullNick", commandProperties = {@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "20")})
+    public String findNick(Integer id) {
+        counterService.increment("invoke");
+        if (id % 3 == 1) {
+            counterService.increment("invoke.error");
+            throw new RuntimeException();
+        }
+        return "nick:" + id;
+    }
+
+    public String nullNick(Integer id) {
+        counterService.increment("fallback");
+        return null;
+    }
+}
