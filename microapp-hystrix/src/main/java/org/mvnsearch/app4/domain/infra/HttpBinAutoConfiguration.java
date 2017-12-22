21dad29e73f69c2410a31f8dc9ce81ff77120071
linux_china <linux_china@hotmail.com>
2017-12-22 12:54:56 +0800

    add httpBinServiceFeignAPI

--- microapp-hystrix/src/main/java/org/mvnsearch/app4/domain/infra/HttpBinAutoConfiguration.java
+++ microapp-hystrix/src/main/java/org/mvnsearch/app4/domain/infra/HttpBinAutoConfiguration.java
@@ -1,6 +1,11 @@
 package org.mvnsearch.app4.domain.infra;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
+import feign.hystrix.HystrixFeign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import feign.slf4j.Slf4jLogger;
 import okhttp3.OkHttpClient;
 import org.mvnsearch.app4.domain.infra.retrofit.HystrixCallAdapterFactory;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +30,15 @@ public class HttpBinAutoConfiguration {
                 addConverterFactory(JacksonConverterFactory.create(objectMapper)).
                 build().
                 create(HttpBinServiceRetrofitAPI.class);
+    }
+
+    @Bean
+    public HttpBinServiceFeignAPI httpBinServiceFeignAPI() {
+        return HystrixFeign.builder()
+                .logger(new Slf4jLogger())
+                .encoder(new JacksonEncoder())
+                .decoder(new JacksonDecoder())
+                .target(HttpBinServiceFeignAPI.class, "http://httpbin.org");
     }
 
     @Bean
