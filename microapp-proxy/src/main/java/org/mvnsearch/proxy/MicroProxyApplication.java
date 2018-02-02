e31fc555dd1031cdca1d63dc2430cd570f242f28
linux_china <linux_china@hotmail.com>
2018-02-03 00:00:11 +0800

    MicroProxyApplication

---
+++ microapp-proxy/src/main/java/org/mvnsearch/proxy/MicroProxyApplication.java
@@ -0,0 +1,26 @@
+package org.mvnsearch.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+public class MicroProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroProxyApplication.class, args);
+    }
+
+    @Bean("charonTaskExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        return executor;
+    }
+
+
+}
