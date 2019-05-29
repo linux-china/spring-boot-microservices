a73a654fbcd80a586b3db02a26299d09472dcc39
linux_china <libing.chen@gmail.com>
2019-05-29 09:10:21 -0700

    hystrix application base test

---
+++ microapp-hystrix/src/test/java/org/mvnsearch/app4/HystrixApplicationBaseTest.java
@@ -0,0 +1,15 @@
+package org.mvnsearch.app4;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * hystrix application base test
+ *
+ * @author linux_china
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HystrixApplicationBaseTest {
+}
